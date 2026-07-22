4eea4a8a4ffa1555dcff7ede2064eba8069ffbfe
Jobaieer <jobaieer.cse@proton.me>
2026-07-23 00:28:18 +0600

    chore: scaffold frontend and backend project with dependencies

---
+++ backend/src/server.js
@@ -0,0 +1,21 @@
+const dns = require("dns");
+dns.setServers(["8.8.8.8", "8.8.4.4"]); 
+
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => res.send("Masjid Namaz Time API running"));
+
+const PORT = process.env.PORT || 5000;
+
+connectDB().then(() => {
+  app.listen(PORT, () =>
+    console.log(` Server running on http://localhost:${PORT}`),
+  );
+});
